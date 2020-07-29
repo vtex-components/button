c64a62afd0a1e809463d9d8ca293d5bcae4409dc
gtkatakura <gt.katakura@gmail.com>
2020-07-29 19:07:28 -0300

    Utility types Props and Components to reuse in another packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,15 @@
-import React, { forwardRef, Ref, PropsWithoutRef } from 'react'
+import React, { forwardRef, Ref } from 'react'
 import { Button as A11yButton, ButtonProps as A11yProps } from 'reakit'
-import { PropsWithAs, As } from 'reakit-utils/types'
 import {
   Button as ThemeAwareButton,
   ButtonProps as ThemeAwareProps,
 } from 'theme-ui'
+import { As, Props, Component } from '@vtex-components/types'
 
-type Component<DefaultAs extends As, DefaultProps> = {
-  <T extends As = DefaultAs>(
-    props: PropsWithAs<PropsWithoutRef<DefaultProps>, T>
-  ): JSX.Element
-}
-
-type ButtonProps = A11yProps & { as?: As }
+type ButtonProps<T extends As = typeof ThemeAwareButton> = Props<T, A11yProps>
 
 const Button = (
-  { as = ThemeAwareButton, ...props }: ButtonProps,
+  { as = ThemeAwareButton, ...props }: ButtonProps<As>,
   ref: Ref<As>
 ) => {
   return <A11yButton ref={ref} as={as} {...props} />
@@ -23,7 +17,4 @@ const Button = (
 
 export { ButtonProps, A11yProps, ThemeAwareProps }
 
-export default forwardRef(Button) as Component<
-  typeof ThemeAwareButton,
-  A11yProps
->
+export default forwardRef(Button) as Component<ButtonProps>
